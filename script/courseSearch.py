8ab3c5cc364283e1f8cee7f0d125209ef197283e
ridemountainpig <lin0905580802@gmail.com>
2022-11-29 00:12:59 +0800

    refactor : get course function

--- script/courseSearch.py
+++ script/courseSearch.py
@@ -2,41 +2,53 @@ import requests
 
 s = requests.Session()
 
+# system year
+year = '111'
+system = '1'
 
-def getCourse():
-    # 通識 mode = 2
-    mode = 2
-    data = '{"baseOptions":{"lang":"cht","year":111,"sms":1},"typeOptions":{"code":{"enabled":false,"value":""},"weekPeriod":{"enabled":false,"week":"*","period":"*"},"course":{"enabled":false,"value":""},"teacher":{"enabled":false,"value":""},"useEnglish":{"enabled":false},"useLanguage":{"enabled":false,"value":"01"},"specificSubject":{"enabled":true,"value":"1"},"courseDescription":{"enabled":false,"value":""}}}'
 
-    # 課程代碼 mode = 2
-    # mode = 2
-    # data = '{"baseOptions":{"lang":"cht","year":111,"sms":1},"typeOptions":{"code":{"enabled":true,"value":"1406"},"weekPeriod":{"enabled":false,"week":"*","period":"*"},"course":{"enabled":false,"value":""},"teacher":{"enabled":false,"value":""},"useEnglish":{"enabled":false},"useLanguage":{"enabled":false,"value":"01"},"specificSubject":{"enabled":false,"value":"1"},"courseDescription":{"enabled":false,"value":""}}}'
-
-    # 系所查詢 mode = 1
-    # mode = 1
-    # data = '{"baseOptions":{"lang":"cht","year":111,"sms":1},"typeOptions":{"degree":"1","deptId":"CI","unitId":"CE07","classId":"CE07134"}}'
-
-    # data["_token"]=token
+def getGeneralCourseList():
+    data = '{"baseOptions":{"lang":"cht","year":' + year + ',"sms":' + system + '},"typeOptions":{"code":{"enabled":false,"value":""},"weekPeriod":{"enabled":false,"week":"*","period":"*"},"course":{"enabled":false,"value":""},"teacher":{"enabled":false,"value":""},"useEnglish":{"enabled":false},"useLanguage":{"enabled":false,"value":"01"},"specificSubject":{"enabled":true,"value":"1"},"courseDescription":{"enabled":false,"value":""}}}'
+    print(data)
     header = {
         "Content-Type":
         "application/json",
         'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     }
-    if mode == 1:
-        res = s.post(
-            "https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType1Result",
-            data=data,
-            headers=header)
-    else:
-        res = s.post(
-            "https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType2Result",
-            data=data,
-            headers=header)
 
-    courseList = res.text.split(r'{\"scr_selcode\":\"')
+    response = s.post(
+        "https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType2Result",
+        data=data,
+        headers=header
+    )
 
-    balanceCourse = {}
+    courseList = response.text.split(r'{\"scr_selcode\":\"')
+    return courseListToDict(courseList)
+   
+
+
+def getCourseByCode(courseCode):
+    data = '{"baseOptions":{"lang":"cht","year":' + year + ',"sms":' + system + '},"typeOptions":{"code":{"enabled":true,"value":"' + str(courseCode) + '"},"weekPeriod":{"enabled":false,"week":"*","period":"*"},"course":{"enabled":false,"value":""},"teacher":{"enabled":false,"value":""},"useEnglish":{"enabled":false},"useLanguage":{"enabled":false,"value":"01"},"specificSubject":{"enabled":false,"value":"1"},"courseDescription":{"enabled":false,"value":""}}}'
+
+    header = {
+        "Content-Type":
+        "application/json",
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    }
+
+    response = s.post(
+        "https://coursesearch02.fcu.edu.tw/Service/Search.asmx/GetType2Result",
+        data=data,
+        headers=header
+    )
+
+    courseList = response.text.split(r'{\"scr_selcode\":\"')
+    return courseListToDict(courseList)
+
+def courseListToDict(courseList):
+    result = {}
     count = 0
     for i in range(1, len(courseList)):
         courseData = courseList[i].split(r'\",\"')
@@ -47,7 +59,7 @@ def getCourse():
         courseBlance = courseData[9].split(r'\":')[2].split(r',')[0]
 
         if courseBlance < courseSum:
-            balanceCourse[count] = {
+            result[count] = {
                 "courseNumber": courseNumber,
                 "courseName": courseName,
                 "courseDate": courseDate,
@@ -55,4 +67,5 @@ def getCourse():
                 "courseSum": courseSum
             }
             count += 1
-    return balanceCourse
+    return result
+
