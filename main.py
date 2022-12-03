47b8e79c59bbd3772d6bea620753ba5b74fd8faa
ridemountainpig <lin0905580802@gmail.com>
2022-12-03 16:28:26 +0800

    feat : add general studies page route

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/generalstudies')
+def generalstudies():
+    return render_template('generalStudies.html', courseData=cs.getGeneralCourseList())
+
 
 app.run(host='0.0.0.0', port=81)
