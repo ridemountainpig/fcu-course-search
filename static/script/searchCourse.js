74d7c76f36b6ebe1885d5b9f9f30c6d0efe0de64
ridemountainpig <lin0905580802@gmail.com>
2023-02-22 00:03:58 +0800

    feat: add course list data html and reset function

--- static/script/searchCourse.js
+++ static/script/searchCourse.js
@@ -1,4 +1,87 @@
 function searchCourse() {
-    $('#searchBox').removeClass('-skew-x-[25deg]');
+    let courseNumber = $('#courseInput').val();
+
+    if (courseNumber == "") {
+        return
+    }
+
+    $('#searchBox').removeClass('-skew-x-[10deg]');
     $('#searchInput').addClass('hidden');
+    $('#courseInput').val("");
+
+    $.ajax({
+        url: "/searchcourse/" + courseNumber,
+        success: function (res) {
+            if (res != 'false') {
+                let courseData = res;
+                let courseHtml = `
+                        <div class="items-center justify-around py-6 px-10 bg-white rounded-xl shadow-2xl">
+                            <div>
+                                <div id="courseNumber" class="flex justify-center items-center p-5 m-3 -mt-14 text-gray-600 font-extrabold text-lg bg-slate-100 rounded-lg">
+                                ${courseData[0].courseNumber}
+                                </div>
+                                <div class="flex justify-end">
+                                    <div class="w-10 h-10 bg-orange-400 text-white rounded-full flex justify-center items-center -mr-14 -mt-16 cursor-pointer" onclick="resetSearchCourse()">
+                                        <i class="bi bi-x-lg" style=" font-size: 22px;"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-start ml-5 mt-5" style="width: 200px;">
+                                <div>
+                                    <div id="courseName" class="text-gray-600 font-bold text-xl">
+                                        ${courseData[0].courseName}
+                                    </div>
+                                    <div id="courseTime" class="text-gray-400 font-medium text-sm my-2">
+                                        上課時間：${courseData[0].courseDate}
+                                    </div>
+                                    <div id="courseTime" class="text-gray-400 font-medium text-sm my-2">
+                                        上課教室：${courseData[0].courseClass}
+                                    </div>
+                                    <div id="courseTime" class="text-gray-400 font-medium text-sm my-2">
+                                        教師：${courseData[0].courseTeacher}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-start min-[930px]:justify-center items-center text-orange-400 font-bold text-xl mt-2">
+                                <div>${courseData[0].courseBalance}<i class="bi bi-lightning-fill px-2"></i>${courseData[0].courseSum}</div>
+                            </div>
+                            <div class="flex justify-center">
+                                <a href="${courseData[0].courseIntroduceUrl}" target="_blank" class="p-5 m-3 text-gray-600 font-bold text-lg rounded-lg cursor-pointer" style="background-color: #dfe7d5;">課程大綱</a>
+                            </div>
+                        </div>
+                    `;
+                $('#courseList').append(courseHtml);
+            } else {
+                let courseHtml = `
+                        <div class="items-center justify-around py-6 px-10 bg-white rounded-xl shadow-2xl">
+                            <div>
+                                <div id="courseNumber" class="flex justify-center items-center p-5 m-3 -mt-14 text-gray-600 font-extrabold text-lg bg-slate-100 rounded-lg">
+                                ${courseNumber}
+                                </div>
+                                <div class="flex justify-end">
+                                    <div class="w-10 h-10 bg-orange-400 text-white rounded-full flex justify-center items-center -mr-14 -mt-16 cursor-pointer" onclick="resetSearchCourse()">
+                                        <i class="bi bi-x-lg" style=" font-size: 22px;"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-start mt-5">
+                                <div>
+                                    <div id="courseName" class="text-gray-600 font-bold text-xl">
+                                        課程號碼錯誤，查無此課程
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                $('#courseList').append(courseHtml);
+            }
+        }
+    })
+
+}
+
+function resetSearchCourse() {
+    $('#searchBox').addClass('-skew-x-[10deg]');
+    $('#searchInput').removeClass('hidden');
+    $('#courseList').empty();
 }
