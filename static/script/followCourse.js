27968a5c99b31f188901a40194904d9e46337989
ridemountainpig <lin0905580802@gmail.com>
2023-06-16 11:02:31 +0800

    refactor: change get course data way

--- static/script/followCourse.js
+++ static/script/followCourse.js
@@ -47,7 +47,7 @@ function addFollowCourse() {
                 setTimeout(function () {
                     $('#courseNumError').addClass('-mr-28');
                 }, 3000);
-                
+
                 $('#coffeeIcon').addClass('hidden');
                 $('#coffeeBackground').addClass('hidden');
                 $('#coffeeIcon').removeClass('w-full h-full');
@@ -90,42 +90,56 @@ function generateCourseList() {
         } else {
             $('#followNumber').append(followList.length);
         }
-        for (let i = followList.length - 1; i >= 0; i--) {
-            if (followList[i] == '') continue
-            $.ajax({
-                async: false,
-                url: "/searchcourse/" + followList[i],
-                success: function (courseData) {
+
+        var data = {
+            followList: followList
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/searchcourselist",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+
+            success: function (courseData) {
+                for (let i = Object.keys(courseData).length - 1; i >= 0; i--) {
                     let courseHtml = `
-                        <div class="min-[1301px]:flex items-center justify-around p-2 my-2 bg-white rounded-xl">
-                            <div id="courseNumber" class="flex justify-center items-center p-5 m-3 text-gray-600 font-extrabold text-lg bg-slate-100 rounded-lg">
-                                ${courseData[0].courseNumber}
-                            </div>
-                            <div class="flex justify-start max-[1301px]:ml-5 min-[1301px]:w-64">
-                                <div>
-                                    <div id="courseName" class="text-gray-600 font-bold min-[1301px]:text-lg text-base">
-                                        ${courseData[0].courseName}
-                                    </div>
-                                    <div id="courseTime" class="text-gray-400 font-medium min-[1301px]:text-sm text-xs">
-                                        ${courseData[0].courseDate}
-                                    </div>
-                                    <div id="courseTime" class="text-gray-400 font-medium min-[1301px]:text-sm text-xs">
-                                        ${courseData[0].courseClass}
+                            <div class="min-[1301px]:flex items-center justify-around p-2 my-2 bg-white rounded-xl">
+                                <div id="courseNumber" class="flex justify-center items-center p-5 m-3 text-gray-600 font-extrabold text-lg bg-slate-100 rounded-lg">
+                                    ${courseData[i].courseNumber}
+                                </div>
+                                <div class="flex justify-start max-[1301px]:ml-5 min-[1301px]:w-64">
+                                    <div>
+                                        <div id="courseName" class="text-gray-600 font-bold min-[1301px]:text-lg text-base">
+                                            ${courseData[i].courseName}
+                                        </div>
+                                        <div id="courseTime" class="text-gray-400 font-medium min-[1301px]:text-sm text-xs">
+                                            ${courseData[i].courseDate}
+                                        </div>
+                                        <div id="courseTime" class="text-gray-400 font-medium min-[1301px]:text-sm text-xs">
+                                            ${courseData[i].courseClass}
+                                        </div>
                                     </div>
                                 </div>
+                                <div class="flex justify-center items-center text-orange-400 font-bold text-xl mt-2">
+                                    <div>${courseData[i].courseBalance}<i class="bi bi-lightning-fill px-2"></i>${courseData[0].courseSum}</div>
+                                </div>
+                                <div class="flex justify-center">
+                                    <button class="p-3 m-3 text-gray-600 font-bold text-base rounded-lg cursor-pointer bg-red-200" onclick="deleteFollowCourse(${courseData[0].courseNumber})">取消關注</button>
+                                </div>
                             </div>
-                            <div class="flex justify-center items-center text-orange-400 font-bold text-xl mt-2">
-                                <div>${courseData[0].courseBalance}<i class="bi bi-lightning-fill px-2"></i>${courseData[0].courseSum}</div>
-                            </div>
-                            <div class="flex justify-center">
-                                <button class="p-3 m-3 text-gray-600 font-bold text-base rounded-lg cursor-pointer bg-red-200" onclick="deleteFollowCourse(${courseData[0].courseNumber})">取消關注</button>
-                            </div>
-                        </div>
-                    `;
+                        `;
                     $("#courseList").append(courseHtml);
                 }
-            })
-        }
+
+                setTimeout(function () {
+                    $('#coffeeIcon').removeClass('w-full h-full');
+                    $('#coffeeBackground').removeClass('w-full h-full');
+                    $('#coffeeIcon').addClass('hidden');
+                    $('#coffeeBackground').addClass('hidden');
+                }, 500);
+            }
+        })
     } else {
         let courseHtml = `
             <div class="flex items-center justify-around p-10 my-2 bg-white rounded-xl font-bold text-xl min-[555px]:text-3xl text-gray-600">
@@ -136,11 +150,12 @@ function generateCourseList() {
         $('#followNumber').empty();
         $('#followNumber').append(0);
         $('#courseList').html(courseHtml);
+
+        setTimeout(function () {
+            $('#coffeeIcon').removeClass('w-full h-full');
+            $('#coffeeBackground').removeClass('w-full h-full');
+            $('#coffeeIcon').addClass('hidden');
+            $('#coffeeBackground').addClass('hidden');
+        }, 500);
     }
-    setTimeout(function () {
-        $('#coffeeIcon').removeClass('w-full h-full');
-        $('#coffeeBackground').removeClass('w-full h-full');
-        $('#coffeeIcon').addClass('hidden');
-        $('#coffeeBackground').addClass('hidden');
-    }, 1500);
 }
