034014f4f8d9e6a4bfb15e88fdf95cf62c45db97
ridemountainpig <lin0905580802@gmail.com>
2023-08-14 14:17:52 +0800

    style: update scroll up icon layout

--- static/script/generalStudies.js
+++ static/script/generalStudies.js
@@ -41,7 +41,7 @@ function getGeneralStudies(courseData) {
                     </div>
                 </div>
                 <div class="flex justify-center max-[930px]:w-full">
-                    <div class="flex justify-center items-center text-orange-400 font-bold text-base min-[930px]:text-xl min-[930px]:w-26 w-20 w-fit mt-2">
+                    <div class="flex justify-center items-center text-orange-400 font-bold text-base min-[930px]:text-xl min-[930px]:w-26 w-fit mt-2">
                         <div class="flex justify-center w-1/3">
                             <div>${courseData[i].courseBalance}</div>
                         </div>
